bc0446a9bd11f9420b591b49286ce40d3bcb7711
callmeabdul743 <abdullahkhalidmul@gmail.com>
2024-08-21 11:38:55 +0500

    Add the randon value

--- lotter.cpp
+++ lotter.cpp
@@ -2,5 +2,22 @@
 #include <stdlib.h>
 using namespace std;
 main(){
-    
+    int person[20];
+    int num;
+    //declare the randon number to the 
+    for(int a=0; a<19; a++){
+        num=rand()%100+1;
+        //remove duplicy
+        for(int j=0; j<=a, j++){
+            if(num==person[a]){
+                j=0;
+                num=rand()%100+1;
+            }
+        }
+        person[a]=num;
+    }
+    //print the array
+    for(int i=0; i<20; i++){
+        cout<<"The person at "<<i+1<<person[i]<<endl;
+    }
 }
