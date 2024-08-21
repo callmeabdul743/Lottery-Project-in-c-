3922019819823dbbb909101f80b5a26e3a772a6b
callmeabdul743 <abdullahkhalidmul@gmail.com>
2024-08-21 11:53:41 +0500

    Completed the project

--- lotter.cpp
+++ lotter.cpp
@@ -4,9 +4,11 @@ using namespace std;
 main(){
     int person[20];
     int num;
+
     //declare the randon number to the 
-    for(int a=0; a<19; a++){
+    for(int a=0; a<20; a++){
         num=rand()%100+1;
+
         //remove duplicy
         for(int j=0; j<=a; j++){
             if(num==person[a]){
@@ -17,9 +19,20 @@ main(){
         person[a]=num;
     }
 
-    
+    //arranging the array
+    int temp;
+    for(int a=0; a<20; a++){
+        for(int j=a+1; j<20; j++){
+            if(person[a]>person[j]){
+                temp=person[a];
+                person[a]=person[j];
+                person[j]= temp;
+            }
+        }
+    }
+
     //print the array
     for(int i=0; i<20; i++){
-        cout<<"The person are: "<<person[i]<<"   "<<i<<endl;
+        cout<<"Congratulation to the lottery NO: "<<person[i]<<", you win the lottery"<<endl;
     }
 }
